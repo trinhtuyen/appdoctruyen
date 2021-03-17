382e1b637b481727e4439cb426316c2357b1a26c
trinhtuyen <dngatay@gmail.com>
2021-03-18 00:57:15 +0700

    tuyen
    123

--- app/src/main/java/com/example/da1_appdoctruyen/MainActivity.java
+++ app/src/main/java/com/example/da1_appdoctruyen/MainActivity.java
@@ -1,9 +1,19 @@
 package com.example.da1_appdoctruyen;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.da1_appdoctruyen.Screen.HomeFragment;
+import com.example.da1_appdoctruyen.Screen.LichSuFragment;
+import com.example.da1_appdoctruyen.Screen.MoreFragment;
+import com.example.da1_appdoctruyen.Screen.ThuVienFragment;
+import com.example.da1_appdoctruyen.Screen.YeuThichFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 private ActionBar toolbar;
@@ -11,8 +21,36 @@ private ActionBar toolbar;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        toolbar= getSupportActionBar();
-        toolbar.setTitle("Trang Chu");
-        loadFragment;
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frag_container, new HomeFragment()).commit();
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFrag = null;
+            switch (item.getItemId()){
+                case R.id.nav_home:
+                    selectedFrag  = new HomeFragment();
+                    break;
+                case R.id.nav_library:
+                    selectedFrag  = new ThuVienFragment();
+                    break;
+                case R.id.nav_history:
+                    selectedFrag  = new LichSuFragment();
+                    break;
+                case R.id.nav_favorite:
+                    selectedFrag  = new YeuThichFragment();
+                    break;
+                case R.id.nav_more:
+                    selectedFrag  = new MoreFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.frag_container,selectedFrag).commit();
+            return true ;
+
+        }
+    };
+
 }
